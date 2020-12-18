df8d00341ea50e29b3b43c33297ec09ee1c4cc24
MackenzieAmoako23 <831590@mystma.org>
2020-12-18 15:46:00 +0000

    1- -4 TopFiveMovies -1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(wordOne, wordTwo, wordThree, wordFour, wordFive) 
+    return wordOne+""+wordTwo+""+wordThree+""+WordFour+""+wordFive
+print topFiveMovies("shrek", "shrek 2", "shrek 3", "shrek 4", "shrek5" ) 
+
