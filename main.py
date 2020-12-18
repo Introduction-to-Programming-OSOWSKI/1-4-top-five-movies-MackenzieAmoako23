72814a0adc1c3e896255552ea60a42b248dafd48
MackenzieAmoako23 <831590@mystma.org>
2020-12-18 16:03:56 +0000

    1-4 topFiveMovies -2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def topFiveMovies(wordOne, wordTwo, wordThree, wordFour, wordFive) 
-    return wordOne+""+wordTwo+""+wordThree+""+WordFour+""+wordFive
-print topFiveMovies("shrek", "shrek 2", "shrek 3", "shrek 4", "shrek5" ) 
+def topFiveMovies(wordOne, wordTwo, wordThree, wordFour, wordFive):
+    return wordOne+" "+wordTwo+" "+wordThree+" "+wordFour+" "+wordFive
 
+print (topFiveMovies("a", "b", "c", "d", "e"))
